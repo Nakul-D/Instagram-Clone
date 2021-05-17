0a769148d6c7f6b316bb03ff54ed0fa3ecabe15b
Nakul Dighe <nakuldigheacc@gmail.com>
2021-05-17 06:19:54 +0530

    Search screen UI complete

--- app/lib/screens/searchScreen.dart
+++ app/lib/screens/searchScreen.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:app/logic/databaseBloc.dart';
+import 'package:app/logic/databaseEvents.dart';
+import 'package:app/screens/profileScreen.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   
   final DatabaseBloc databaseBloc;
   
   SearchScreen({@required this.databaseBloc});
 
   @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchController = TextEditingController();
+
+  search() {
+    // This function will search the typed user
+    print("Search");
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Search screen"),
+      appBar: AppBar(
+        title: Text("Search"),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(12.0, 2.0, 12.0, 2.0),
+                      child: TextFormField(
+                        controller: searchController,
+                        decoration: InputDecoration(
+                          hintText: "Search a user",
+                          border: InputBorder.none,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(100.0)
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: () => search(),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(100.0)
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [],
+              )
+            )
+          ],
+        ),
       ),
     );
   }
